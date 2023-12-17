5b501a458aac933c2b6aedd23f610429db2feae2
Arun1only1 <arun1nly1@gmail.com>
2023-12-17 16:07:49 +0545

    constant in js

---
+++ r.assignment.and.constant.in.js.object.js
@@ -0,0 +1,38 @@
+// const person1 = {
+//   firstName: "Alish",
+//   email: "al@gmail.com",
+//   age: 22,
+// };
+
+// person1.firstName = "Prakash";
+// person1.gender = "male";
+
+// console.log(person1);
+
+// constant => which cannot be re-assigned
+
+// const person2 = {
+//   firstName: "X",
+// };
+
+// person2 = {
+//   firstName: "Y",
+// };
+
+// person2.firstName = "Y";
+// console.log(person2);
+
+// const firstName = "Rohil";
+// firstName = "Rohit";
+
+//? primitive data types => constant means cannot be changed(immutable)
+// string
+// number
+// boolean
+// null
+// undefined
+
+// ? non-primitive => constant means cannot be reassigned  but can be changed (mutable)
+// object
+
+// In both cases constant means cannot be re-assign
