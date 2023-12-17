01ea49b8251c5b5835b8c96d7e50961d240274aa
Arun1only1 <arun1nly1@gmail.com>
2023-12-17 16:07:27 +0545

    array reduce,forEach, for..of

---
+++ p.reduce.js
@@ -0,0 +1,65 @@
+let numList = [100, 200, 250, 75, 65];
+
+// for (const item of numList) {
+//   console.log(item);
+// }
+
+// let sum = 0;
+// numList.forEach((item) => {
+//   sum = sum + item;
+// });
+
+// console.log(sum);
+
+// const result = numList.reduce((sum, item, index, self) => {
+//   sum = sum + item;
+
+//   return sum;
+// }, 0);
+
+// console.log(result);
+
+// let product = 1;
+// numList.forEach((item, index, self) => {
+//   product = product * item;
+// });
+
+// console.log(product);
+
+// const result = numList.reduce((product, item) => {
+//   product = product * item;
+
+//   return product;
+// }, 1);
+
+// console.log(result);
+
+// const productList = [
+//   {
+//     name: "A",
+//     price: 20,
+//   },
+//   {
+//     name: "B",
+//     price: 15,
+//   },
+//   {
+//     name: "C",
+//     price: 7,
+//   },
+//   {
+//     name: "D",
+//     price: 49,
+//   },
+// ];
+
+// let sum = 0;
+// productList.forEach((item, index, self) => {
+//   sum = sum + item.price;
+// });
+
+// console.log(sum);
+
+// const result = productList.reduce((sum, item) => sum + item.price, 0);
+
+// console.log(result);
